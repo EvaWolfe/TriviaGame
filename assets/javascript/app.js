5325598352676b9a128ab88f55dcfa78745d5bce
Eva <ol0.badwolf.0lo@gmail.com>
2017-10-08 22:56:04 -0400

    Updated timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,7 +46,7 @@ var game = {
     }
   },
   start: function() {
-    // timer = setInterval(game.countdown, 1000);
+    timer = setInterval(game.countdown, 1000);
     $('#subWrapper').prepend('<h2> Time Remaining: <span id= "counter"> 60 </span> Seconds </h2>')
     $('#start').remove();
     for (var i = 0; i < questions.length; i++) {
